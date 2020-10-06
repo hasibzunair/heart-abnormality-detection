8bef93e36aef094638bd01aee6dc1f4d1305d48b
hasibzunair <hasibzunair@gmail.com>
2020-10-06 19:35:59 -0400

    script to convert per beat to image

--- signalToImage.py
+++ signalToImage.py
@@ -6,27 +6,37 @@ beat according to its class separately.
 from glob import glob
 import wfdb
 import numpy as np
-from tqdm import tqdm
 import matplotlib.pyplot as plt
 import cv2
 import os
 import math
 
-def get_records():
+# Prevent progress bar from printing to new line
+from functools import partial
+from tqdm import tqdm
+tqdm = partial(tqdm, position=0, leave=True)
+
+ROOT_DIR = os.path.abspath("./")
+DATA_PATH = os.path.join(ROOT_DIR, "datasets", "mit-bih-arrhythmia-database-1.0.0/")
+DATA_PATH
+
+def get_records(patients_idxs):
     """ Get paths for data in data/mit/ directory """
     # Download if doesn't exist
 
     # There are 3 files for each record
     # *.atr is one of them
-    paths = glob('./datasets/mit-bih-arrhythmia-database-1.0.0/*.atr')
+    #paths = glob('{}/*.atr'.format(DATA_PATH))
 
     # Get rid of the extension
-    paths = [path[:-4] for path in paths]
-    paths.sort()
-
+    #paths = [path[:-4] for path in paths]
+    #paths.sort()
+    
+    paths = [os.path.join(DATA_PATH, path) for path in patients_idxs]
+    
     return paths
 
-def segmentation(records, type, output_dir=''):
+def segmentation(records, type, class_counter, output_dir=''):
 
     """'N' for normal beats. Similarly we can give the input 'L' for left bundle branch block beats. 'R' for right bundle branch block
         beats. 'A' for Atrial premature contraction. 'V' for ventricular premature contraction. '/' for paced beat. 'E' for Ventricular
@@ -60,7 +70,7 @@ def segmentation(records, type, output_dir=''):
     floor = mean_v - 3*std_v
     ceil = mean_v + 3*std_v
 
-    for e in tqdm(records):
+    for e in records:
         signals, fields = wfdb.rdsamp(e, channels = [0])
 
         ann = wfdb.rdann(e, 'atr')
@@ -83,29 +93,90 @@ def segmentation(records, type, output_dir=''):
                 for spine in plt.gca().spines.values():
                     spine.set_visible(False)
 
-                filename = output_dir + 'fig_{}'.format(count) + '.png'
+                filename = output_dir + 'fig_{}_{}'.format(class_counter, count) + '.png'
                 plt.savefig(filename)
                 plt.close()
                 im_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
                 im_gray = cv2.erode(im_gray, kernel, iterations=1)
                 im_gray = cv2.resize(im_gray, (192, 128), interpolation=cv2.INTER_LANCZOS4)
                 cv2.imwrite(filename, im_gray)
-                print('img writtten {}'.format(filename))
+                #print('img writtten {}'.format(filename))
                 count += 1
-
-
+                
     return results
 
 
 
-if __name__ == "__main__":
-    records = get_records()
-    """'N' for normal beats. Similarly we can give the input 'L' for left bundle branch block beats. 'R' for right bundle branch block
-        beats. 'A' for Atrial premature contraction. 'V' for premature ventricular contraction. '/' for paced beat. 'E' for Ventricular
-        escape beat."""
-    labels = ['N', 'L', 'R', 'A', 'V', '/', 'E', '!']
-    output_dirs = ['NOR/', 'LBBB/', 'RBBB/', 'APC/', 'PVC/', 'PAB/', 'VEB/', 'VFE/']
+pts_train = ['222',
+ '107',
+ '101',
+ '118',
+ '116',
+ '115',
+ '108',
+ '106',
+ '214',
+ '105',
+ '219',
+ '205',
+ '102',
+ '232',
+ '220',
+ '114',
+ '228',
+ '117',
+ '121',
+ '100',
+ '231',
+ '234',
+ '124',
+ '122',
+ '202',
+ '217',
+ '212',
+ '233']
 
-    for type, output_dir in zip(labels, output_dirs):
-        sgs = segmentation(records, type, output_dir='./datasets/MIT-BIH_AD/'+output_dir)
+pts_val = ['210', '223', '200', '103', '113', '207', '119', '213', '112', '109']
+pts_test = ['104', '111', '123', '201', '203', '208', '209', '215', '221', '230']
 
+print("Patient instances in train, val and test: ", len(pts_train), len(pts_val), len(pts_test))
+
+train_records = get_records(pts_train)
+val_records = get_records(pts_val)
+test_records = get_records(pts_test)
+
+
+# See https://archive.physionet.org/physiobank/annotations.shtml
+# Get beats from https://arxiv.org/pdf/1804.06812.pdf
+
+# From the MIT-BIH  database, we  includednormal  beat  (NOR)  and  seven  types
+# of  ECG  arrhythmias  including  prema-ture ventricular contraction (PVC),
+# paced beat (PAB), right bundle branchblock beat (RBB), left bundle branch block beat (LBB),
+# atrial premature con-traction (APC), ventricular flutter wave (VFW), and ventricular escape beat(VEB). 
+
+# NOR PVC PAB RBB LBB APC VFW VEB
+
+# 'N' for normal beats
+# 'L' for left bundle branch block beats 
+# 'R' for right bundle branch block
+# 'A' for Atrial premature contraction
+# 'V' for premature ventricular contraction 
+# '/' for paced beat
+# 'E' for Ventricular escape beat
+# '!' for Ventricular flutter wave
+
+labels = ['N', 'L', 'R', 'A', 'V', '/', 'E', '!']
+#labels = ['N', 'L','R','V','/','A','f','F','j','a','E','J','e','S']
+output_dirs = ['NOR/', 'LBBB/', 'RBBB/', 'APC/', 'PVC/', 'PAB/', 'VEB/', 'VFE/']
+#output_dirs = ['0/', '1/', '1/', '1/', '1/', '1/', '1/', '1/']
+
+train_records = train_records[:3]
+
+# Training data
+
+class_counter = 0
+for type, output_dir in tqdm(zip(labels, output_dirs)):
+    output_dir = os.path.join("train", output_dir)
+    segmentation(train_records, type, class_counter, output_dir='./datasets/isolated-beat-images/'+output_dir)
+    class_counter+=1
+    
